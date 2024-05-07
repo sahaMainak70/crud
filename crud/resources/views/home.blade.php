f2c129b7e4a187ef7d07d842d6b4850f31e74d60
sahaMainak70 <sahamainak70@gmail.com>
2024-05-07 22:09:43 +0530

    chore: Update navbar styling and add responsive menu toggle

--- crud/resources/views/home.blade.php
+++ crud/resources/views/home.blade.php
@@ -6,31 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Crud Website</title>
-    <link rel="shortcut icon" href="{{ asset('images/9.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/11.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
     <div class="navbar">
         <div class="logo">
-            <a href="{{ route('home') }}"><img src="{{ asset('images/11.png') }}" alt="Logo"></a>
+            <img src="{{ asset('images/10.png') }}" alt="">
+        </div>
+        <div class="menu-toggle">
+            <i class="fa-solid fa-bars"></i>
         </div>
         <div class="menu">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Create</a></li>
-                <li><a href="#">Read</a></li>
+                <li><a href="">Read</a></li>
                 <li><a href="#">Update</a></li>
                 <li><a href="#">Delete</a></li>
             </ul>
         </div>
     </div>
-    
-    <div class="container">
-        <div class="name">
-            <img src="{{ asset('images/10.png') }}" alt="name">
-        </div>
-    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
